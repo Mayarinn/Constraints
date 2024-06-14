08cf87afd6f90845f60124cb81f6fa31f9a41271
Karina Osadchaya <dyach.zero@gmail.com>
2024-06-14 11:37:16 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Maÿarinn Tindómë on 6/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
